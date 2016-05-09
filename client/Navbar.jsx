1454292a296f909d441d291a1f0396436efbc238
Isaac Lee <kioshuu3371@gmail.com>
2016-05-08 19:32:10 -0700

    initialized custom views for login

--- client/Navbar.jsx
+++ client/Navbar.jsx
@@ -14,68 +14,53 @@ Navbar = React.createClass({
 		event.preventDefault();
 		this.hidePage();
 		if(Meteor.user()==null){
-			ReactDOM.render(<AutoSlider />, document.getElementById('render-quad1'))
-		ReactDOM.render(<HomeText />, document.getElementById('render-quad2'))
+		    ReactDOM.render(<HomeText />, document.getElementById('render-quad2'))
 			} else {
 			ReactDOM.render(<BucketList />, document.getElementById('render-quad1'));
-  	  // ReactDOM.render(<CategoriesReact />, document.getElementById("render-quad2"));
   	  ReactDOM.render(<Map />, document.getElementById("render-quad3"));
   	}
 	},
 
-  renderBucketList (event) {
+  renderBucketList(event) {
   	event.preventDefault();
 		this.hidePage();
 		ReactDOM.render(<BucketList />, document.getElementById('render-quad1'));
     // ReactDOM.render(<CategoriesReact />, document.getElementById("render-quad2"));
   },
 
-	renderEvents (event) {
+	renderEvents(event) {
 		event.preventDefault();
 		this.hidePage();
 		ReactDOM.render(<Events />, document.getElementById('render-quad2'));
 	},
 
-	// render (){
-	// 	return (
-	// 		<nav className="navbar">
-	// 			<ul className="nav nav-tabs">
-	// 				<li role="presentation"><a href="#" onClick={this.renderHome}>Home</a></li>
-	// 				<li role="presentation"><a href="#" onClick={this.renderBucketList}>Bucket List</a></li>
-	// 				<li role="presentation"><a href="#" onClick={this.renderEvents}>Events</a></li>
-	// 				<li role="presentation"><a href="#" onClick={this.renderFriends}>Friends</a></li>
-	// 			</ul>
-	// 		</nav>
-	// 	)
-	// }
 	renderLandingPage(event){
 		event.preventDefault();
 		this.hidePage();
-		ReactDOM.render(<AutoSlider />, document.getElementById('render-home'));
 		ReactDOM.render(<HomeText />, document.getElementById('render-quad2'));
 	},
 
+  renderLogin(event){
+    event.preventDefault();
+    this.hidePage();
+    ReactDOM.render(<Login />, document.getElementById('render-quad2'));
+  },
+
 	render (){
 		return (
-			<nav className="navbar navbar-inverse navbar-fixed-top">
-			  <div className="container-fluid">
+			<nav className="red" role="navigation">
+			  <div className="nav-wrapper container">
 			    <div className="navbar-header">
-			      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-			        <span className="icon-bar"></span>
-			        <span className="icon-bar"></span>
-			        <span className="icon-bar"></span>
-			      </button>
-			      <a onClick={this.renderLandingPage} className="navbar-brand glyphicon glyphicon-fire" href="#"></a>
-			    </div>
-			    <div className="collapse navbar-collapse" id="myNavbar">
-
-			      <ul className="nav navbar-nav">
-							<li className= "Active" role="presentation"><a href="#" onClick={this.renderHome}>Home</a></li>
-							<li role="presentation"><a href="#" onClick={this.renderBucketList}>Personal Bucket List</a></li>
-							<li role="presentation"><a href="#" onClick={this.renderEvents}>Map</a></li>
-			      </ul>
-			      	<span id="render-login" className="pull-right"></span>
-			    </div>
+          <ul className="left">
+            <li className="Active waves-effect waves-light btn" onClick={this.renderLandingPage}><a href="#"><i className="material-icons">view_list</i></a></li>
+            <li className="waves-effect waves-light btn" onClick={this.renderHome}><a href="#" >Home</a></li>
+            <li className="waves-effect waves-light btn" onClick={this.renderBucketList}><a href="#" >Personal Bucket List</a></li>
+            <li className="waves-effect waves-light btn" onClick={this.renderEvents}><a href="#" >Map</a></li>
+          </ul>
+          <ul className="right">
+            <li className="waves-effect waves-light btn" onClick={this.renderLogin}><a href="#" >Login</a></li>
+          </ul>
+          </div>
 			  </div>
 			</nav>
 		)
--- client/login.jsx
+++ client/login.jsx
@@ -1,15 +1,53 @@
 Login = React.createClass({
 
-  hasUsername() {
-    Accounts.ui.config({
-      passwordSignupFields: "USERNAME_ONLY"
+  submitRegistration(event) {
+    event.preventDefault();
+    var emailVar = ReactDOM.findDOMNode(this.refs.registerEmail).value.trim();
+    var passwordVar = ReactDOM.findDOMNode(this.refs.registerPassword).value.trim();
+    Accounts.createUser({
+      email: emailVar,
+      password: passwordVar
     });
   },
 
+  submitLogin(event) {
+    event.preventDefault();
+    var emailVar = ReactDOM.findDOMNode(this.refs.loginEmail).value.trim();
+    var passwordVar = ReactDOM.findDOMNode(this.refs.loginPassword).value.trim();
+    Meteor.loginWithPassword(emailVar, passwordVar);
+  },
+
   render() {
-    {this.hasUsername()}
+  if(Meteor.user() == null) {
     return(
-      <AccountsUIWrapper />
-    )
+      <div>
+      <h2> Please log in. </h2>
+        <form onSubmit={this.submitLogin}>
+          <p>Email:
+          <input  type="email" ref="loginEmail"/>
+          </p>
+          <p>Password:
+          <input type="password" ref="loginPassword"/>
+          </p>
+          <input type="submit" value="Login" />
+        </form>
+
+      <h2> No account? No problem. Make one now! </h2>
+        <form onSubmit={this.submitRegistration}>
+          <p>Email:
+          <input type="email" ref="registerEmail"/>
+          </p>
+          <p>Password
+          <input type="password" ref="registerPassword"/>
+          </p>
+          <input type="submit" value="Register"/>
+        </form>
+      </div>
+      )
+    } else {
+      return(
+      <div> HELLO WORLD </div>
+      )
+    }
   }
 });
